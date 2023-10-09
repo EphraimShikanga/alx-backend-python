982fd8f54d35d5cc62713a8d0a855795527a066f
EphraimShikanga <ephraimshikanga@gmail.com>
2023-10-09 23:24:56 +0300

    Multiple couroutines

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
-
+import asyncio
+from typing import List
+from functools import cmp_to_key
 
 wait_random = __import__('0-basic_async_syntax.py').wait_random
 
-async def wait_n(n, max_delay):
-    
+async def wait_n(n, max_delay) -> List[float]:
+    """[summary]
+
+    Args:
+        n (int): [description]
+        max_delay ([type]): [description]
+
+    Returns:
+        List[float]: [description]
+    """
+    delays = []
+
+    async def collect_delay(index: int):
+        delay = await wait_random(max_delay)
+        delays.append((index, delay))
+
+    coroutines = [collect_delay(i) for i in range(n)]
+    await asyncio.gather(*coroutines)
+
+    delays.sort(key=cmp_to_key(lambda a, b: a[0] - b[0]))
+    return [delay for _, delay in delays]
