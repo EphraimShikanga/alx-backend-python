604c80ee111e4064bbae70cc7777052de8f6ca0c
EphraimShikanga <ephraimshikanga@gmail.com>
2023-10-11 20:38:52 +0300

    Run time for parallel comprehension

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,13 @@
+#!/
+"""Measure the runtime"""
+import asyncio
+
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+async def measure_runtime() -> float:
+    """Measure the runtime"""
+    start = asyncio.get_running_loop().time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    end = asyncio.get_running_loop().time()
+    return end - start
